c8a10526acb2c6e7efdd226ffb9f0d3efb28b3a1
SHYIRAMBERE Emmanuel <110242932+EmmanuelSHYIRAMBERE@users.noreply.github.com>
2025-01-29 17:30:21 +0200

    Fix build conflicts

--- frontend/src/dashboard/challenge/ChallengeDetails.tsx
+++ frontend/src/dashboard/challenge/ChallengeDetails.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import ProjectHeader from "./ProjectHeader";
 import RequirementsList from "./RequirementsList";
